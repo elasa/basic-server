b46859292481fb74a40ed0fc4e13b9b67fe11349
elasa <erik.lasa.t@gmail.com>
2016-06-11 00:32:55 -0300

    create a functions for callback and events on server

---
+++ server.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const http = require('http')
+
+// you can use enviroment variable: PORT=8080 node server.js on command line
+const port = process.env.PORT || 3000
+
+const server = http.createServer()
+
+// EventEmiter
+server.on('request', testRequest)
+server.on('listening', testlistening)
+
+server.listen(port)
+
+// Funciotions for callback
+function testlistening(){
+  console.log('listening on port '+port);
+}
+
+function testRequest(req, res){
+  res.end('Hello io.js');
+}
